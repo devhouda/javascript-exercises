3f8df19ea3dbf3c451ae0e8a219f7cb6de2d9da2
devhouda <houdadjemai.1@gmail.com>
2026-04-15 11:36:43 +0100

    remove from array

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,6 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, ...arg) {
+  let newArray = arr.filter((el) => !arg.includes(el));
+  return newArray;
 };
 
 // Do not edit below this line
