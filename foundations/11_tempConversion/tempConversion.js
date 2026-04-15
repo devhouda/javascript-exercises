a3da9caf684021dcc945fd1407aa7fd5523f717a
devhouda <houdadjemai.1@gmail.com>
2026-04-15 13:06:24 +0100

    temperture conversion from celsuis to fahrenheit and vice versa

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,11 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (temp) {
+  let tempInCelsius = ((temp - 32) * 5) / 9;
+  let roundedTempInCelsius = Math.round(tempInCelsius * 10) / 10;
+  return roundedTempInCelsius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (temp) {
+  let tempInFahrenheit = temp * (9 / 5) + 32;
+  let roundedTempInFahrenheit = Math.round(tempInFahrenheit * 10) / 10;
+  return roundedTempInFahrenheit;
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
