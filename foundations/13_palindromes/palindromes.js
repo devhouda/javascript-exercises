5de74820cee6cf055e58f908b2a32e31e7a5a84b
devhouda <houdadjemai.1@gmail.com>
2026-04-17 19:53:56 +0100

    detect palindromes and non palindromes strings

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  let cleanStr = str.replace(/[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/g, "");
+  cleanStr = cleanStr.replaceAll(" ", "");
+  cleanStr = cleanStr.toLowerCase();
+  let reversed = cleanStr.split("").reverse().join("");
+  if (cleanStr === reversed) return true;
+  else return false;
 };
 
 // Do not edit below this line
