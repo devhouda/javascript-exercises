c693370fa2cb81b0f4038c525fce5b5f805fc2ff
devhouda <houdadjemai.1@gmail.com>
2026-04-17 19:23:38 +0100

    calculator exercise

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,44 @@
-const add = function() {
-	
+const add = function (num1, num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function (num1, num2) {
+  return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function (arr) {
+  let sum = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+
+  return sum;
 };
 
-const multiply = function() {
+const multiply = function (arr) {
+  let res = 1;
 
+  for (let i = 0; i < arr.length; i++) {
+    res *= arr[i];
+  }
+
+  return res;
 };
 
-const power = function() {
-	
+const power = function (num1, num2) {
+  return num1 ** num2;
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+  let factorial = 1;
+  if (num === 0 || num === 1) return 1;
+  let i = num;
+  while (i > 1) {
+    factorial *= i;
+    i--;
+  }
+  return factorial;
 };
 
 // Do not edit below this line
@@ -29,5 +48,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
