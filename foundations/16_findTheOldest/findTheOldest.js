e10df528d283b505692a172e3e01998e3e66aebe
devhouda <houdadjemai.1@gmail.com>
2026-04-18 11:16:49 +0100

    find the oldest person in an array of objects

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,30 @@
-const findTheOldest = function() {
+const findTheOldest = function (arr) {
+  // const items = [
+  //   { name: "Apple", price: 10 },
+  //   { name: "Banana", price: 20 },
+  //   { name: "Cherry", price: 15 },
+  // ];
 
+  const oldest = arr.reduce((prev, curr) => {
+    // let age;
+    // if (prev.yearOfDeath) {
+    //   age = prev.yearOfDeath - prev.yearOfBirth;
+    // } else {
+    //   age = new Date().getFullYear() - prev.yearOfBirth;
+    // }
+    if (prev.yearOfDeath) {
+      return prev.yearOfDeath - prev.yearOfBirth >
+        curr.yearOfDeath - curr.yearOfBirth
+        ? prev
+        : curr;
+    } else {
+      let currentYear = new Date().getFullYear();
+      return currentYear - prev.yearOfBirth > currentYear - curr.yearOfBirth
+        ? prev
+        : curr;
+    }
+  });
+  return oldest;
 };
 
 // Do not edit below this line
